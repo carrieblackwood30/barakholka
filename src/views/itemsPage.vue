69946c82643cdbe7c68eac833b753d0fc2cc70bb
unknown <carrieblackwood30@gmail.com>
2024-10-14 12:31:09 +0500

    added netlify file

--- src/views/itemsPage.vue
+++ src/views/itemsPage.vue
@@ -1,7 +1,21 @@
 <template>
-    {{ $route.params.item }}
+    <div>
+        <div v-for="good in itemsPageGood" :key="good.id">
+            <div v-if="good.header === $route.params.item">
+                {{ good.description }}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
+    import { goods } from '@/lib/stores/goods';
+    import { onBeforeMount, ref } from 'vue';
 
+    const itemsPageGood = ref()
+
+    onBeforeMount(async() =>{
+        await goods.init()
+        itemsPageGood.value = goods.current
+    })
 </script>
